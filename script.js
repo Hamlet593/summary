d2b1538c29d06bd31b976a09b443b619966ba7e9
Hamlet593 <hamomuradyan93@gmail.com>
2021-06-04 17:21:52 +0400

    setTimeout

--- script.js
+++ script.js
@@ -168,3 +168,20 @@ let promise = new Promise(function(resolve, reject) {
   // Использование:
   let user = new User("Hovo");
   user.sayHi();
+
+//   SetTimeout
+
+function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      console.log(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+  printNumbers(5, 10);
+
+  //
