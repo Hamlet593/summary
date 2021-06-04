b5dab301dfe3d39a44637e20cf5ebb0967f809ab
Hamlet593 <hamomuradyan93@gmail.com>
2021-06-04 17:27:56 +0400

    Async/Await

--- script.js
+++ script.js
@@ -207,3 +207,18 @@ function printNumbers(from, to) {
   
   let user2 = JSON.stringify(user)
   console.log(user)
+
+  //AsyncAwait
+
+  async function loadJson(url) { // (1)
+    let response = await fetch(url); // (2)
+  
+    if (response.status == 200) {
+      let json = await response.json(); // (3)
+      return json;
+    }
+  
+    throw new Error(response.status);
+  }
+  
+  loadJson('Չկա նման user').catch(console.log);
